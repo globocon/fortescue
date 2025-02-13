648f354f6a2d59f2ebcd5ff89fbd5a9329961113
Dileep Sebastian <42341364+dill3032@users.noreply.github.com>
2025-02-13 15:24:19 +0530

    Add project files.

---
+++ FortescueWebApp/Pages/Forms/EngineeringData.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FortescueWebApp.Pages.Forms
+{
+    public class EngineeringDataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
